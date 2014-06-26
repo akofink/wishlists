e453d152dc90f7bb9ba817321d762164d26e0351
Andrew Kofink <ajkofink@gmail.com>
2014-06-25 22:23:52 -0400

    Begin setting up main static bits of site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def last_page
+    request.env['HTTP_REFERER']
+  end
+  helper_method :last_page
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :page
+
+  get '/about', to: 'pages', id: 'about', as: 'about'
+
+  root "page#welcome"
 end
