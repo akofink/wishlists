7e5c570d089a105c0392d8fa543b403778dd206f
Andrew Kofink <ajkofink@gmail.com>
2014-06-26 23:32:17 -0400

    Set up new user and login form views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,33 @@ class UsersController < ApplicationController
   def new
   end
 
+  def create
+  end
+
   def edit
+  end
+
+  def update
   end
 
   def show
   end
 
   def index
+  end
+  
+  private
+
+  def user_params
+    params
+      .require(:user)
+      .allow(
+        :user_name,
+        :email,
+        :first_name,
+        :last_name,
+        :password,
+        :password_confirmation
+      )
   end
 end
